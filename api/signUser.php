92bc725eb9cf7ffce3da44783d74799adf4d015d
BastienRoques <bastien.roques971@gmail.com>
2019-03-16 19:56:56 +0100

    edit signUser script

--- api/signUser.php
+++ api/signUser.php
@@ -9,37 +9,37 @@
     $password = $_POST["password"];
     $passwordconfirm = $_POST["passwordconfirm"];
  
-    // $message = "";
-    // $firstnameErr = "";
-    // $lastnameErr = "";
-    // $usernameErr = "";
 
-    if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['username'])  && isset($_POST['password']) && isset($_POST['passwordconfirm'])){
-        // $username_verify_req = "SELECT COUNT(*) FROM users WHERE username = :username LIMIT 1";
-        // if( $res = $db->query($username_verify_req) ){
-        //     if($res->fetchColumn() > 0){
-        //         // $message = "Votre pseudo est déjà utilisé.";
-        //         echo 'pseudo déjà utilisé';
-        //     }else{
-                $req = $db->prepare("INSERT INTO users (firstname, lastname, username, password, passwordconfirm) VALUES (:firstname, :lastname, :username, :password, :passwordconfirm)");
-                
-                try{
-                    $req->execute(array(
-                        "firstname" => $_POST["firstname"],
-                        "lastname" => $_POST["lastname"],
-                        "username" => $_POST["username"],
-                        "password" => $_POST["password"],
-                        "passwordconfirm" => $_POST["passwordconfirm"]
-                    ));
-                    
-                    $return_arr[] = array(
-                        "username" => $username,
-                        "password" => $password);
-                    echo json_encode($return_arr);
-                }catch (Exception $e){
-                    echo "error";
-                }
-            
+    if(isset($firstname) && isset($lastname) && isset($username) && isset($password) && isset($passwordconfirm)){
+
+        $q = $db->prepare('SELECT COUNT(username) AS num FROM users WHERE username = :username');
+        $q->bindParam(":username", $username);
+        $q->execute();
+        $row = $q->fetch(PDO::FETCH_ASSOC);
         
-    }
+        // Vérifie si le pseudo est déjà utilisé
+        if($row['num'] > 0){
+            die('That username already exists!');
+        // Sinon inscription de l'utilisateur dans la base de donnnée
+        }else{
+            $q = $db->prepare("INSERT INTO users (firstname, lastname, username, password, passwordconfirm) VALUES (:firstname, :lastname, :username, :password, :passwordconfirm)");
+            
+            try{
+                $q->bindParam(":firstname", $firstname);
+                $q->bindParam(":lastname", $lastname);
+                $q->bindParam(":username", $username);
+                $q->bindParam(":password", $password);
+                $q->bindParam(":passwordconfirm", $passwordconfirm);
+                $result = $q->execute();
+    
+                if( $result ){
+                    echo json_encode($result);
+                }else{
+                    echo json_encode( [] );
+                }
+            }catch (Exception $e){
+                echo "error";
+            }  
+        }   
+    }   
 ?>
